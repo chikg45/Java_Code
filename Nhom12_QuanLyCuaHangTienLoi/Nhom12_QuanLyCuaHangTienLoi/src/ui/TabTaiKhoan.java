9b28489d87a9bbba3b03058b73c6aa240830cb7c
chikg45 <tai332006@gmail.com>
2026-05-07 02:40:03 +0700

    Update TabTaiKhoan.java

--- Nhom12_QuanLyCuaHangTienLoi/Nhom12_QuanLyCuaHangTienLoi/src/ui/TabTaiKhoan.java
+++ Nhom12_QuanLyCuaHangTienLoi/Nhom12_QuanLyCuaHangTienLoi/src/ui/TabTaiKhoan.java
@@ -1,7 +1,96 @@
-package ui;
-
-import javax.swing.JPanel;
-
-public class TabTaiKhoan extends JPanel{
-		// TODO Auto-generated constructor stub
-}
+	package ui;
+	
+	import java.awt.*;
+	import javax.swing.*;
+	
+	import ui.UIKhachHang;
+	import ui.UIQuanLyNhanVien;
+	
+	public class TabTaiKhoan extends JPanel {
+		
+		private CardLayout cardLayout;
+		private JPanel pnlCard;
+		private JComboBox<String> cbLoai;
+		
+	    public TabTaiKhoan() {
+	       
+	    	setLayout(new BorderLayout());
+	         add(createMainContent(), BorderLayout.CENTER);
+	     
+	    }
+	    
+	    private JPanel createTitle() {
+	    	JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+	        panel.setBackground(Color.WHITE);
+	
+	        JLabel lblTitle = new	 JLabel("TÀI KHOẢN");
+	        lblTitle.setFont(new Font("Arial", Font.BOLD, 24));
+	        lblTitle.setForeground(new Color(21, 82, 120));
+	
+	        String[] types = { "Khách hàng", "Nhân viên" };
+	        cbLoai = new JComboBox<>(types);
+	
+	        cbLoai.addActionListener(e -> {
+	            String selected = (String) cbLoai.getSelectedItem();
+	            cardLayout.show(pnlCard, selected); 
+	        });
+	        
+	        panel.add(lblTitle);
+	        panel.add(cbLoai);
+	
+	        return panel;
+	    	
+	    }
+	    
+	    private JPanel createMainContent() {
+	        JPanel panel = new JPanel(new BorderLayout());
+	        
+	        cardLayout = new CardLayout();
+	        pnlCard = new JPanel(cardLayout);
+	        
+	        panel.add(createTitle(),BorderLayout.NORTH);
+	       
+	        
+	        
+	        UIKhachHang  guiKhachHang = new UIKhachHang();
+	        UIQuanLyNhanVien guiNhanVien = new UIQuanLyNhanVien();
+	        
+	        
+	      
+	        
+	        pnlCard.add(guiKhachHang,"Khách hàng");    
+	        pnlCard.add(guiNhanVien,"Nhân viên");
+	        
+	        
+	        panel.add(pnlCard,BorderLayout.CENTER);
+	        
+	        return panel;
+	    }
+	    
+	    private JButton createMenuButton(JButton btn,String text, boolean active) {
+	    	 
+	        
+	        btn.setMaximumSize(new Dimension(200, 60));
+	      
+	
+	          btn.setAlignmentX(Component.LEFT_ALIGNMENT);
+	          btn.setHorizontalAlignment(SwingConstants.LEFT);
+	
+	          btn.setForeground(Color.WHITE);
+	          btn.setFont(new Font("Segoe UI", Font.BOLD, 16));
+	
+	          btn.setFocusPainted(false);
+	          btn.setBorderPainted(false);
+	
+	          if (active) {
+	              btn.setBackground(new Color(0, 188, 212)); // xanh sáng
+	          } else {
+	              btn.setBackground(new Color(21, 82, 120)); // xanh đậm
+	          }
+	
+	          btn.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 10));
+	
+	          return btn;
+	      }
+	    
+	}
